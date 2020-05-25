7c7558d4b8d52bc6682f52d488e7890305103e97
lhx <lu18300766903@163.com>
2020-05-25 16:16:00 +0800

    提交本地工程

---
+++ git-test-project/src/com/lhx/git/FirstClass.java
@@ -0,0 +1,18 @@
+package com.lhx.git;
+
+/**
+ * @Author lhx
+ * @Date 2020/5/25 10:34
+ * @Version 1.0
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("helloworld git");
+        System.out.println("helloworld git");
+        System.out.println("helloworld git");
+        System.out.println("helloworld git");
+        System.out.println("helloworld git");
+        System.out.println("helloworld git");
+
+    }
+}
